d5dde540a9f6238588f8d7e863d7a39bea9decb3
TZUHSIEN-TSAI-ACS <tzuhsien.tsai.z2@mail.toray>
2025-06-23 14:54:23 +0900

    fixing color ver1.0

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,51 @@ import authProvider from './authProvider';
 import LoginPage from './LoginPage.jsx'; 
 import NewPasswordPage from './NewPasswordPage.jsx';
 
+import indigo from '@mui/material/colors/indigo';
+import pink from '@mui/material/colors/pink';
+import red from '@mui/material/colors/red';
+
+
+const myTheme = {
+  ...defaultTheme,
+  palette: {
+      mode: 'dark',
+      primary: indigo,
+      secondary: pink,
+      error: red,
+  },
+  typography: {
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Arial',
+          'sans-serif',
+      ].join(','),
+  },
+  components: {
+      ...defaultTheme.components,
+      MuiTextField: {
+          defaultProps: {
+              variant: 'outlined' as const,
+          },
+      },
+      MuiFormControl: {
+          defaultProps: {
+              variant: 'outlined' as const,
+          },
+      },
+  },
+};
+
+
 export const App = () => (
+
+
   <Admin 
     layout={Layout} 
+    theme={theme}
     dataProvider={dataProvider} 
     authProvider={authProvider} 
     loginPage={LoginPage}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@mui/material/colors/indigo',
+      '@mui/material/colors/pink', // 也把您使用的其他颜色模块加进去
+      '@mui/material/colors/red',
+    ],
+  },
   server: {
     host: true,
   },
